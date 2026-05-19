649eeba5287df4597e374b7943a917a9090c4893
sean <sean@macbookpro.home>
2026-05-19 21:17:08 +0100

    Add main.c using Telecommand struct for TC_101

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include "satellite.h" 
 
 int main() {
     printf("Satellite simulator online\n");
+
+    Telecommand systemCheck;
+    
+    systemCheck.commandID = 101;
+
+    printf("System Check Command: %d\n", systemCheck.commandID);
+
     return 0;
 }
