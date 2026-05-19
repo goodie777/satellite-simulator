91b04866a362f36ee381c8b5e35091f7de90f24b
sean <sean@macbookpro.home>
2026-05-19 22:11:45 +0100

    Add TC_101 and TC_102 Telecommand structs with printf output

--- main.c
+++ main.c
@@ -2,13 +2,21 @@
 #include "satellite.h" 
 
 int main() {
-    printf("Satellite simulator online\n");
 
     Telecommand systemCheck;
+    Telecommand adjustOrbitalPath;
     
     systemCheck.commandID = 101;
+    
+    adjustOrbitalPath.commandID = 102;
+    adjustOrbitalPath.parameters[0] = 1.5;
+    adjustOrbitalPath.parameters[1] = 2.5;
+    adjustOrbitalPath.parameters[2] = 7.5;
+
 
     printf("System Check Command: %d\n", systemCheck.commandID);
+    printf("Adjust Orbital Path: %.2f, %.2f, %.2f\n", adjustOrbitalPath.parameters[0],adjustOrbitalPath.parameters[1],adjustOrbitalPath.parameters[2]);
+
 
     return 0;
 }
