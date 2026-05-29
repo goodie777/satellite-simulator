cfd75a504181c8b8ded609a1540290faa1f45cda
sean <sean@macbookpro.home>
2026-05-29 19:40:41 +0100

    Update ground and structures - TC_102

--- ground.py
+++ ground.py
@@ -5,3 +5,11 @@ data = TC_101(101)
 print(data.command_id)
 
 
+data2 = TC_102(102, 2.5,7.4,8.7)
+
+print(data2.command_id)
+print(data2.DeltaVx)
+print(data2.DeltaVy)
+print(data2.DeltaVz)
+
+
--- structures.py
+++ structures.py
@@ -4,8 +4,13 @@ from ctypes import *
 class TC_101(Structure):
     _fields_ = [("command_id", c_uint32)]
 
-#class TC_102(Structure):
- #   _fields_ = [("command_id", c_uint32, deltaVx: float, DeltaVy: float, DeltaVz: float)]
+class TC_102(Structure):
+   _fields_ = [
+    ("command_id", c_uint32),
+    ("DeltaVx", c_float),
+    ("DeltaVy", c_float),
+    ("DeltaVz", c_float)
+    ]
 
 class TC_103(Structure):
     _fields_ = [("command_id", c_uint32)]
