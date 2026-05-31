8325ad996081bd034d943f142846f2e550f36d92
sean <sean@macbookpro.home>
2026-05-31 14:36:58 +0100

    Add TM_201 to ground.py

--- ground.py
+++ ground.py
@@ -18,3 +18,12 @@ data3 = TC_103(7,16,58)
 print(data3.command_id)
 print(data3.operation_code)
 print(data3.payload_id)
+
+# TM 
+
+TM_data1 = TM_201(201,7.5,8.9,10.0 )
+
+print(TM_data1.status_code)
+print(TM_data1.cpu_usage)
+print(TM_data1.memory_usage )
+print(TM_data1.battery_level)
